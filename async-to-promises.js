cccd9d200f031b810262024ffc35ba8dc9184bf0
Ryan Petrich <rpetrich@gmail.com>
2018-02-23 14:48:52 -0500

    Assume that undefined doesn't search scope

--- async-to-promises.js
+++ async-to-promises.js
@@ -1645,6 +1645,9 @@ exports.default = function({ types, template, traverse }) {
 	}
 
 	function identifierSearchesScope(path) {
+		if (path.node.name === "undefined") {
+			return false;
+		}
 		const parent = path.parentPath;
 		if (parent.isVariableDeclarator() && parent.get("id") === path) {
 			return false;

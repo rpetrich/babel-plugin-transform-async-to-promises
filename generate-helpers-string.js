d795742cec84233000e32eeb264cd562b9a2fab5
Ryan Petrich <rpetrich@gmail.com>
2021-12-22 13:22:45 -0500

    Generate an ES5 compatible CommonJS helpers.js from the source helpers.mjs

--- generate-helpers-string.js
+++ generate-helpers-string.js
@@ -1,4 +1,16 @@
 const { readFileSync, writeFileSync } = require("fs");
+const { parse, transformSync } = require("@babel/core")
+const generator = require("@babel/generator").default;
 
-const helperCode = readFileSync("helpers.js").toString();
+const helperCode = readFileSync("helpers.mjs").toString();
+
+const { code } = transformSync(helperCode, {
+	sourceType: "module",
+	compact: true,
+	minified: true,
+	plugins: ["@babel/plugin-transform-modules-commonjs"],
+	presets: ["@babel/preset-env"],
+});
+
+writeFileSync("helpers.js", code);
 writeFileSync("helpers-string.js", `exports.__esModule = true;\nexports.code = ${JSON.stringify(helperCode)};\n`);
